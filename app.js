0cb7f5f257be471513df51c1c43a86dce27c8565
etnik2002 <46058633+etnik2002@users.noreply.github.com>
2022-10-04 19:04:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const MongoStore = require('connect-mongo');
 const passport = require('passport');
 const path = require('path');
 const User = require('./controllers/User-Controller');
+
 require('dotenv').config();
 //importojm routes
-const PORT = process.env.PORT;
-
 const mongoose = require('mongoose');
+
+const PORT = process.env.PORT;
 
 const fs = require('fs');
 
 const Product = require('./models/Product');
-const Reklama = require('./models/Reklamo');
 const Category = require('./models/Category');
+const Reklama = require('./models/Reklamo');
 
 //import and connect to database
 const lidhuMeDb = require('./db');
@@ -26,7 +27,6 @@ const categoryRoutes = require('./routes/categories');
 const productRoutes = require('./routes/products');
 const reklamaRoutes = require('./routes/reklamo');
 const { kerkohetIdentifikimi, isAdmin } = require('./middleware/auth');
-// const { dashboard } = require('./controllers/User-Controller');
 
 // middlewares
 
@@ -69,15 +69,10 @@ app.use('/reklamo', reklamaRoutes);
 
 app.get('/', async (req, res) => {
   const isFeatured = await Product.find({ isFeatured: true });
+  const categories = await Category.find({}).lean();
 
   console.log(isFeatured);
-  res.render('homepage', { isFeatured });
-});
-
-//html file
-
-app.get('/html', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.render('homepage', { isFeatured, categories });
 });
 
 app.get('/:id', async (req, res) => {
@@ -91,7 +86,13 @@ app.get('/:id', async (req, res) => {
   }
 });
 
-app.post('getProds', (req, res) => {
+//html file
+
+app.get('/html', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/getProds', (req, res) => {
   let payload = req.body.payload.trim();
   console.log(payload);
 });
